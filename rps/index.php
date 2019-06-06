5d0fe44fc67d10fa844a3b85e249872a1a58b80d
VIMAL KUMAR <46487476+vimalverma558@users.noreply.github.com>
2019-06-06 17:06:05 +0530

    rps
    rps

---
+++ rps/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>VIMAL KUMAR - 0a117f2c</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="https://github.com/vimalverma558/coursera-test/tree/master/rps"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
+
